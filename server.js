5398ce42372195088625e52d8bcb8e33261f98a9
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-07-24 23:16:28 +0530

    added new functions

--- server.js
+++ server.js
@@ -32,6 +32,47 @@ app.get("/", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Server started at Port 5000");
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+  // socket.off("setup", () => {
+  //   console.log("USER DISCONNECTED");
+
+  // });
+
+  socket.on("disconnect", (userData) => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+    // Handle any necessary cleanup or disconnection logic here
+  });
 });
